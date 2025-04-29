630b1ffde1cff3d8e65a0298aa91090fc798d46b
urbanrentals <info@urbanrentals.nyc>
2025-04-29 00:27:12 -0400

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // you only need this if you really want to prevent bundling
-      external: []
-    }
-  }
-});
+
+  // Force production bundles for React & React Router
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
+})
